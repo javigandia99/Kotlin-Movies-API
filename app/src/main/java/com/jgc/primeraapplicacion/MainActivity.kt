8007e4e8eae5df43fc6e794d7b543956c92bf4f9
javier <javigandia99@gmail.com>
2019-10-03 14:48:38 +0200

    Init Count

--- app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
+++ app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.IntegerRes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val  helloText: TextView = findViewById(R.id.hellotext)
+        var  helloText: TextView = findViewById(R.id.hellotext)
 
         val myButton: Button = findViewById(R.id.mybutton)
+        var count = 0
         myButton.setOnClickListener{
-            var message = getString(R.string.button_pressed)
+            var message = getString(R.string.button_pressed) + count
             Toast.makeText(MainActivity@this,message,Toast.LENGTH_LONG).show()
-
+            count++
+            helloText.text = count.toString()
 
         }
     }
