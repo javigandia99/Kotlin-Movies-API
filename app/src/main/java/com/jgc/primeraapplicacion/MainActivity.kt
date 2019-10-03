a1ba8a6f48b9b5234083e9094897d1c56cdec18f
javier <javigandia99@gmail.com>
2019-10-03 19:50:12 +0200

    Button and text with count ready

--- app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
+++ app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
@@ -5,22 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.IntegerRes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var  helloText: TextView = findViewById(R.id.hellotext)
-
+        var  countText: TextView = findViewById(R.id.count_text)
+        countText.text = getString(R.string.counttext)
         val myButton: Button = findViewById(R.id.mybutton)
         var count = 0
         myButton.setOnClickListener{
-            var message = getString(R.string.button_pressed) + count
+            var message = getString(R.string.button_pressed)
             Toast.makeText(MainActivity@this,message,Toast.LENGTH_LONG).show()
             count++
-            helloText.text = count.toString()
+           countText.text = count.toString()
 
         }
     }
