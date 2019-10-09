ab847ce8ca6157c7eb6e3b686b2b905f9510b811
javier <javigandia99@gmail.com>
2019-10-09 11:32:54 +0200

    Formatted code Ctrl+Alt+L

--- app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
+++ app/src/main/java/com/jgc/primeraapplicacion/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val  countText: TextView = findViewById(R.id.count_textview)
+        val countText: TextView = findViewById(R.id.count_textview)
         countText.text = getString(R.string.count_text)
         val myButton: Button = findViewById(R.id.mybutton)
         var count = 0
-        myButton.setOnClickListener{
+        myButton.setOnClickListener {
             var message = getString(R.string.button_pressed)
-            Toast.makeText(MainActivity@this,message,Toast.LENGTH_LONG).show()
+            Toast.makeText(MainActivity@ this, message, Toast.LENGTH_LONG).show()
             count++
-           countText.text = count.toString()
+            countText.text = count.toString()
 
         }
     }
