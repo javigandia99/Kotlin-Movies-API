29356b55751991a05fac3a9e392c10f6ff03aaf2
javier <javigandia99@gmail.com>
2019-10-11 20:59:06 +0200

    Add Description  and fixed profileActivity
    Add buttonBack to MainActivity
    Checked out that the ScrollView is working
    Add Description Strings
    Change Photo of Valentino Rossi

--- app/src/main/java/com/jgc/primeraapplicacion/ProfileActivity.kt
+++ app/src/main/java/com/jgc/primeraapplicacion/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package com.jgc.primeraapplicacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -10,6 +12,8 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var cityText: TextView
     private lateinit var ageText: TextView
+    private lateinit var descriptionText: TextView
+    private lateinit var buttonBack: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +23,27 @@ class ProfileActivity : AppCompatActivity() {
         imageView = findViewById(R.id.profile_imageView)
         cityText = findViewById(R.id.profile_city)
         ageText = findViewById(R.id.profile_age)
+        descriptionText = findViewById(R.id.profile_description)
+        buttonBack = findViewById(R.id.button_back)
 
         val name = intent.extras?.getString("intent_name")
-        val image = intent.getIntExtra("image", 1)
+        val image = intent.getIntExtra("intent_image", 1)
         val city = intent.extras?.getString("intent_city")
         val age = intent.extras?.getString("intent_age")
+        val description = intent.extras?.getString("intent_description")
 
         nameText.text = name
         imageView.setImageResource(image)
         cityText.text = city
         ageText.text = age
+        descriptionText.text = description
 
 
+
+        buttonBack.setOnClickListener() {
+            val backintent = Intent(this, MainActivity::class.java)
+            startActivity(backintent)
+
+        }
     }
 }
