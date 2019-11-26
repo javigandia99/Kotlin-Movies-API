ee46e6240dc7cf7f596eefdb4af5e02b2b1c4d43
javier <javigandia99@gmail.com>
2019-11-26 17:59:51 +0100

    Design changes

--- app/src/main/java/com/jgc/primeraapplicacion/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/jgc/primeraapplicacion/ui/profile/ProfileFragment.kt
@@ -44,10 +44,10 @@ class ProfileFragment : Fragment(), ProfileView {
         )
         presenter = ProfilePresenter(this, localRepository)
 
-        val name = "Valentino Rossi"
-        val image = R.drawable.valentino_rossi
-        val city = "Tuvullia"
-        val age = "40"
+        val name = "Dory"
+        val image = R.drawable.dory
+        val city = "Australia"
+        val age = "4"
         val description = "Lorem Ipsum is simply dummy text of the printing and t" +
                 "setting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
                 "when an unknown printer took a galley of type and scrambled it to make a type specimen book." +
